f38ea0138ef72483355fa237541b43bdce203493
LalitDhane <lalitdhane4455@gmail.com>
2022-09-11 17:10:52 +0530

    changed db connection code

--- dbconnect.js
+++ dbconnect.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-(async () => {
-  try {
-    await mongoose.connect(process.env.DBURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  } catch (err) {
-    console.log("error : " + err);
-  }
-})();
-
-const connection = mongoose.connection;
-
-connection.once("open", () => console.log("Database is connected...."));
+//Connecting to the Database
+mongoose
+  .connect(process.env.DBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DB.");
+  })
+  .catch((err) => {
+    console.log("Error : " + err);
+  });
